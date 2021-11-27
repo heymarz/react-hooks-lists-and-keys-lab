9b7cf04790db89e9f7b27a765d24e137be4a33a8
Marlena Tsang <Marlena345@gmail.com>
2021-11-26 20:09:50 -0500

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,19 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>
+    <a key={links.home} href="#home">home</a>
+    <a key={links.about} href="#about">about</a>
+    <a key={links.projects} href="#projects">projects</a>
+  </nav>;
 }
 
 export default NavBar;
+
+// In the `NavBar` component, there is an array with three strings representing
+// each link on the page. For each of those strings, create an `<a>` tag that looks
+// like this:
+
+// ```jsx
+// <a href="#home">home</a>
+// ```
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,21 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const technologiesList = technologies.map(tech =>(
+    <span key={tech}>{tech}</span>
+  ))
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+      {technologiesList}
       </div>
     </div>
   );
 }
 
 export default ProjectItem;
+//The `ProjectItem` component should get a prop of `technologies` passed down from
+//`ProjectList`. For each element in this array of technologies, create a `<span>`
+//tag that displays the name of the technology.
